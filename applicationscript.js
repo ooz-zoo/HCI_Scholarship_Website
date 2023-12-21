9c93cfe645b98640795478274651e3475e9f9a70
ooz-zoo <111248086+ooz-zoo@users.noreply.github.com>
2023-12-21 13:58:09 +0300

    commit
    final commit

---
+++ applicationscript.js
@@ -0,0 +1,7 @@
+// Get a reference to the "Submit" button element
+const submitButton = document.getElementById('submitButton');
+
+// Add a click event listener to the button
+submitButton.addEventListener('click', function() {
+    alert('Submission Successful');
+});
